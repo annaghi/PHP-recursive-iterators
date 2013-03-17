e84cc5362cce764c1b48da86d2c3d75d77efb8f2
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-17 12:44:34 +0100

    Create RecursiveArrayIterator::offsetExists.php

---
+++ RecursiveArrayIterator::offsetExists.php
@@ -0,0 +1,56 @@
+<?php
+
+// Simple use case of RecursiveArrayIterator::offsetExists method
+
+function offsetExistsTest() {
+
+    $array = array (
+            'a' => array(
+                    'a_1' => 'a 1 text',
+                    'a_2' => 'a 2 text',
+            ),
+            'b' => array(
+                    'b_1' => 'b 1 text',
+                    'b_2' => 'b 2 text',
+            ),
+    );
+
+//    $object = new ArrayObject( $array, 1, "RecursiveArrayIterator" );
+    $object   = json_decode( json_encode( $array ));
+    $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::SELF_FIRST );
+
+    foreach( $iterator as $key => $current ) {
+
+print_r('*************************************');
+print_r( $iterator->getInnerIterator() );
+
+         if( mb_strlen( $key ) == 1 && ! $iterator->getInnerIterator()->offsetExists( 'new' )) {
+
+print_r('-------------- START ----------------');
+print_r( $iterator->getInnerIterator() );
+
+             $iterator->getInnerIterator()->getChildren()->offsetSet( 'new', 'new text' );
+
+print_r( $iterator->getInnerIterator() );
+print_r('-------------- END ------------------');
+         }
+    }
+
+//print_r($object->getArrayCopy());
+print_r(object_to_array($object));
+}
+
+offsetExistsTest();
+
+
+//helper method
+function object_to_array( $object ) {
+    if( ! is_array( $object ) && ! is_object( $object )) {
+        return $object;
+    }
+    if( is_object( $object )) {
+        $object = get_object_vars( $object );
+    }
+    return array_map( __FUNCTION__, $object );
+}
+
