7efd7b19dd90305ccfd7fe7a151e028f8d932afb
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-18 07:03:01 +0100

    Update RecursiveArrayIterator::offsetExists.php

--- RecursiveArrayIterator::offsetExists.php
+++ RecursiveArrayIterator::offsetExists.php
@@ -38,6 +38,8 @@ function offsetExistsTest() {
     $object   = json_decode( json_encode( $array ));
 //**    $object   = new ArrayObject( $array, 0, "RecursiveArrayIterator" );
     $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::SELF_FIRST );
+//    $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::CHILD_FIRST );
+
 
     foreach( $iterator as $key => $current ) {
 
