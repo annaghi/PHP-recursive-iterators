aa055c882ad93fe540b1f3b68cd6f026e7140b44
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-18 09:34:01 +0100

    Update RecursiveArrayIterator::offsetSet.php

--- RecursiveArrayIterator::offsetSet.php
+++ RecursiveArrayIterator::offsetSet.php
@@ -25,7 +25,7 @@ function offsetSetTest() {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    $object   = json_decode( json_encode( $array ));
+    $object   = json_decode( json_encode( $array_rec ));
 //**    $object   = new ArrayObject( $array, 0, "RecursiveArrayIterator" );
     $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::SELF_FIRST );
 
@@ -33,16 +33,27 @@ function offsetSetTest() {
 
         if( mb_strlen( $key ) == 1 ) {
 
-            $iterator->getInnerIterator()->getChildren()->offsetSet( 'new', 'new text' );
+            $iterator->getInnerIterator()->getChildren()->offsetSet( 'new', 'Jekyll' );
 
         }
     }
 
+print_r($object);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+    foreach( $iterator as $key => $current ) {
+
+        if( $key == 'new') {
+
+            $iterator->getInnerIterator()->offsetSet( $key, 'Hide' );
+        }
+    }
+
 print_r($object);
-//**print_r($object->getArrayCopy());
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
 
@@ -56,14 +67,14 @@ stdClass Object
 (
     [a] => stdClass Object
         (
-            [new] => new text
+            [new] => Jekyll
         )
 
     [b] => stdClass Object
         (
             [b_1] => b 1 text
             [b_2] => b 2 text
-            [new] => new text
+            [new] => Jekyll
         )
 
     [c] => stdClass Object
@@ -78,7 +89,39 @@ stdClass Object
                     [c_2_2] => c 2 2 text
                 )
 
-            [new] => new text
+            [new] => Jekyll
+        )
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+stdClass Object
+(
+    [a] => stdClass Object
+        (
+            [new] => Hide
+        )
+
+    [b] => stdClass Object
+        (
+            [b_1] => b 1 text
+            [b_2] => b 2 text
+            [new] => Hide
+        )
+
+    [c] => stdClass Object
+        (
+            [c_1] => stdClass Object
+                (
+                )
+
+            [c_2] => stdClass Object
+                (
+                    [c_2_1] => c 2 1 text
+                    [c_2_2] => c 2 2 text
+                )
+
+            [new] => Hide
         )
 )
 
