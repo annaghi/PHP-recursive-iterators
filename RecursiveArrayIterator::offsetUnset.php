1113fa2c5259b2d130166cb3ed4bc8e2651ec4bd
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-18 08:32:41 +0100

    Create RecursiveArrayIterator::offsetUnset.php

---
+++ RecursiveArrayIterator::offsetUnset.php
@@ -0,0 +1,77 @@
+<?php
+
+// Very simple use case of RecursiveArrayIterator::offsetUnset method
+
+function offsetUnsetTest() {
+
+    $array = array (
+            'a' => new stdClass(),
+//**            'a' => array(),
+            'b' => array(
+                    'b_1' => 'b 1 text',
+                    'b_2' => 'b 2 text',
+            ),
+            'c' => array(
+                    'c_1' => new stdClass(),
+//**                    'c_1' => array(),
+                    'c_2' => array(
+                            'c_2_1' => 'c 2 1 text',
+                            'c_2_2' => 'c 2 2 text',
+                    ),
+            ),
+    );
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    $object   = json_decode( json_encode( $array ));
+//**    $object   = new ArrayObject( $array, 0, "RecursiveArrayIterator" );
+    $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::SELF_FIRST );
+
+    foreach( $iterator as $key => $current ) {
+
+        if( in_array( $key, array( 'b_1', 'c_1', ))) {
+
+            $iterator->getInnerIterator()->offsetUnset( $key );
+
+        }
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+print_r($object);
+//**print_r($object->getArrayCopy());
+
+}
+
+offsetUnsetTest();
+
+
+/*
+
+stdClass Object
+(
+    [a] => stdClass Object
+        (
+        )
+
+    [b] => stdClass Object
+        (
+            [b_2] => b 2 text
+        )
+
+    [c] => stdClass Object
+        (
+            [c_2] => stdClass Object
+                (
+                    [c_2_1] => c 2 1 text
+                    [c_2_2] => c 2 2 text
+                )
+
+        )
+)
+
+*/
+
