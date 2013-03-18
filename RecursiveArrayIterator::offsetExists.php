c0ef06e4d17f5fc3f7e89e9c453840e7f417b9cf
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-18 17:25:05 +0100

    Update RecursiveArrayIterator::offsetExists.php

--- RecursiveArrayIterator::offsetExists.php
+++ RecursiveArrayIterator::offsetExists.php
@@ -5,20 +5,28 @@
 function offsetExistsTest() {
 
     $array = array (
-            'a' => new stdClass(),
-//**            'a' => array(),
+            'a' => array(
+                    'a_1' => 'a 1 text',
+                    'a_2' => 'a 2 text',
+            ),
+
             'b' => array(
-                    'b_1' => 'b 1 text',
-                    'b_2' => 'b 2 text',
-            ),
-            'c' => array(
-                    'c_1' => new stdClass(),
-//**                    'c_1' => array(),
-                    'c_2' => array(
-                            'c_2_1' => 'c 2 1 text',
-                            'c_2_2' => 'c 2 2 text',
+                    'b_1' => array(
+                            'b_1_1' => 'b 1 1 text',
+                            'b_1_2' => 'b 1 2 text',
+                            'b_1_3' => 'b 1 3 text',
                     ),
+
+                    'b_2' => 0,
+                    'b_3' => '',
+                    'b_4' => array(),
+                    'b_5' => new stdClass(),
             ),
+
+            'c' => 0,
+            'd' => '',
+            'e' => array(),
+            'f' => new stdClass,
     );
 
 
@@ -34,7 +42,6 @@ function offsetExistsTest() {
         if( ! $iterator->getInnerIterator()->offsetExists( 'new' )) {
 
             $iterator->getInnerIterator()->offsetSet( 'new', 'new text' );
-
         }
     }
 
@@ -54,30 +61,43 @@ offsetExistsTest();
 stdClass Object
 (
     [a] => stdClass Object
-        (                                         ?????
+        (
+            [a_1] => a 1 text
+            [a_2] => a 2 text
+            [new] => new text
         )
 
     [b] => stdClass Object
         (
-            [b_1] => b 1 text
-            [b_2] => b 2 text
-            [new] => new text
-        )
-
-    [c] => stdClass Object
-        (
-            [c_1] => stdClass Object
-                (                                 ?????
-                )
-
-            [c_2] => stdClass Object
+            [b_1] => stdClass Object
                 (
-                    [c_2_1] => c 2 1 text
-                    [c_2_2] => c 2 2 text
+                    [b_1_1] => b 1 1 text
+                    [b_1_2] => b 1 2 text
+                    [b_1_3] => b 1 3 text
                     [new] => new text
                 )
 
+            [b_2] => 0
+            [b_3] => 
+            [b_4] => Array
+                (
+                )
+
+            [b_5] => stdClass Object
+                (
+                )
+
             [new] => new text
+        )
+
+    [c] => 0
+    [d] => 
+    [e] => Array
+        (
+        )
+
+    [f] => stdClass Object
+        (
         )
 
     [new] => new text
