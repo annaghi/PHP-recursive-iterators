f96ff9552612a1f7ca6398ce9faea6f029de3550
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-18 08:28:53 +0100

    Update RecursiveArrayIterator::count.php

--- RecursiveArrayIterator::count.php
+++ RecursiveArrayIterator::count.php
@@ -6,13 +6,13 @@ function countTest() {
 
     $array = array (
             'a' => new stdClass(),
-//** 'a' => array(),
+//**            'a' => array(),
             'b' => array(
                     'b_1' => 'b 1 text',
             ),
             'c' => array(
                     'c_1' => new stdClass(),
-//** 'c_1' => array(),
+//**                    'c_1' => array(),
                     'c_2' => array(
                             'c_2_1' => 'c 2 1 text',
                             'c_2_2' => 'c 2 2 text',
@@ -24,8 +24,8 @@ function countTest() {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    $object = json_decode( json_encode( $array ));
-//** $object = new ArrayObject( $array, 0, "RecursiveArrayIterator" );
+    $object   = json_decode( json_encode( $array ));
+//**    $object   = new ArrayObject( $array, 0, "RecursiveArrayIterator" );
     $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::SELF_FIRST );
 
 
