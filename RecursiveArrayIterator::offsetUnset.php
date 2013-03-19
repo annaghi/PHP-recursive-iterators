df393c5c8b2376a02931ab76730b644c77f3f613
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-19 06:25:15 +0100

    Update RecursiveArrayIterator::offsetUnset.php

--- RecursiveArrayIterator::offsetUnset.php
+++ RecursiveArrayIterator::offsetUnset.php
@@ -6,27 +6,40 @@ function offsetUnsetTest() {
 
     $array = array (
             'a' => array(
-                    'a_1' => 'a 1 text',
-                    'a_2' => 'a 2 text',
+                    'a_1' => 'a 1 first',
+                    'a_2' => 'a 2 first',
             ),
-
             'b' => array(
                     'b_1' => array(
-                            'b_1_1' => 'b 1 1 text',
-                            'b_1_2' => 'b 1 2 text',
-                            'b_1_3' => 'b 1 3 text',
+                            'b_1_1' => 'b 1 1 first',
+                            'b_1_2' => 'b 1 2 first',
+                            'b_1_3' => 'b 1 3 first',
                     ),
 
                     'b_2' => 0,
                     'b_3' => '',
                     'b_4' => array(),
                     'b_5' => new stdClass(),
+
+
+                    'a' => array(
+                            'a_1' => 'a 1 second',
+                            'a_2' => 'a 2 second',
+                    ),
+                    'b' => array(
+                            'b_1' => array(
+                                    'b_1_1' => 'b 1 1 second',
+                                    'b_1_2' => 'b 1 2 second',
+                                    'b_1_3' => 'b 1 3 second',
+                            ),
+
+                            'b_2' => 0,
+                            'b_3' => '',
+                            'b_4' => array(),
+                            'b_5' => new stdClass(),
+                    ),
             ),
 
-            'c' => 0,
-            'd' => '',
-            'e' => array(),
-            'f' => new stdClass,
     );
 
 
@@ -35,11 +48,12 @@ function offsetUnsetTest() {
 
     $object   = json_decode( json_encode( $array ));
 //**    $object   = new ArrayObject( $array, 0, "RecursiveArrayIterator" );
-    $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::SELF_FIRST );
+    $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::CHILD_FIRST );
+//    $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::SELF_FIRST );
 
     foreach( $iterator as $key => $current ) {
 
-        if( in_array( $key, array( 'a_1', 'b_1_1', 'b_2', 'b_3', 'b_4', 'b_5' ))) {
+        if( in_array( $key, array( 'a_1', 'b_1', 'b_2', 'b_3', 'b_4', 'b_5' ))) {
 
             $iterator->getInnerIterator()->offsetUnset( $key );
         }
@@ -63,26 +77,21 @@ stdClass Object
 (
     [a] => stdClass Object
         (
-            [a_2] => a 2 text
+            [a_2] => a 2 first
         )
 
     [b] => stdClass Object
         (
-            [b_1] => stdClass Object
+            [b_2] => 0
+            [a] => stdClass Object
                 (
-                    [b_1_2] => b 1 2 text
-                    [b_1_3] => b 1 3 text
+                    [a_2] => a 2 second
                 )
-        )
 
-    [c] => 0
-    [d] => 
-    [e] => Array
-        (
-        )
-
-    [f] => stdClass Object
-        (
+            [b] => stdClass Object
+                (
+                    [b_2] => 0
+                )
         )
 )
 
