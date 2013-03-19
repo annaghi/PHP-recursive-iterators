428d306d17a7f31bef59422335fe9b4af1a0dfc7
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-19 21:24:41 +0100

    Update RecursiveArrayIterator::offsetExists.php

--- RecursiveArrayIterator::offsetExists.php
+++ RecursiveArrayIterator::offsetExists.php
@@ -24,7 +24,7 @@ function offsetExistsTest() {
 
             'e' => array(
 
-                    'a' => 'a first',
+                    'a' => 'a second',
                     'b' => array(
                             'b_1' => 'b_1 second',
                     ),
@@ -104,7 +104,7 @@ stdClass Object
 
     [e] => stdClass Object
         (
-            [a] => a first
+            [a] => a second
             [b] => stdClass Object
                 (
                     [b_1] => b_1 second
