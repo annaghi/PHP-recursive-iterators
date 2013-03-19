2519936bbfe09cf92d1b76fea058e4485113d125
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-19 20:45:45 +0100

    Update RecursiveArrayIterator::offsetSet.php

--- RecursiveArrayIterator::offsetSet.php
+++ RecursiveArrayIterator::offsetSet.php
@@ -4,39 +4,40 @@
 
 function offsetSetTest() {
 
-    $array = array (
-            'a' => array(
-                    'a_1' => 'a 1 first',
-                    'a_2' => 'a 2 first',
-            ),
+     $array = array (
+
+            'a' => 'a first',
             'b' => array(
-                    'b_1' => array(
-                            'b_1_1' => 'b 1 1 first',
-                            'b_1_2' => 'b 1 2 first',
-                            'b_1_3' => 'b 1 3 first',
+                    'b_1' => 'b_1 first',
+            ),
+            'c' => array(
+                    'c_1' => 'c_1 first',
+                    'c_2' => 'c_2 first',
+            ),
+            'd' => array(
+                    'd_1' => array(
+                            'd_1_1' => 'd_1_1 first',
+                            'd_1_2' => 'd_1_2 first',
+                            'd_1_3' => 'd_1_3 first',
                     ),
+            ),
 
-                    'b_2' => 0,
-                    'b_3' => '',
-                    'b_4' => array(),
-                    'b_5' => new stdClass(),
+            'e' => array(
 
-
-                    'a' => array(
-                            'a_1' => 'a 1 second',
-                            'a_2' => 'a 2 second',
-                    ),
+                    'a' => 'a first',
                     'b' => array(
-                            'b_1' => array(
-                                    'b_1_1' => 'b 1 1 second',
-                                    'b_1_2' => 'b 1 2 second',
-                                    'b_1_3' => 'b 1 3 second',
+                            'b_1' => 'b_1 second',
+                    ),
+                    'c' => array(
+                            'c_1' => 'c_1 second',
+                            'c_2' => 'c_2 second',
+                    ),
+                    'd' => array(
+                            'd_1' => array(
+                                    'd_1_1' => 'd_1_1 second',
+                                    'd_1_2' => 'd_1_2 second',
+                                    'd_1_3' => 'd_1_3 second',
                             ),
-
-                            'b_2' => 0,
-                            'b_3' => '',
-                            'b_4' => array(),
-                            'b_5' => new stdClass(),
                     ),
             ),
     );
@@ -53,7 +54,7 @@ function offsetSetTest() {
 
     foreach( $iterator as $key => $current ) {
 
-        if( mb_strlen( $key ) == 1 && ! $iterator->getInnerIterator()->offsetExists( 'new' )) {
+        if( $iterator->getInnerIterator()->count() == 3 && ! $iterator->getInnerIterator()->offsetExists( 'new' )) {
 
             $iterator->getInnerIterator()->offsetSet( 'new', 'Jekyll' );
         }
@@ -72,7 +73,7 @@ print_r($object);
             $iterator->getInnerIterator()->offsetSet( $key, 'Hyde' );
         }
     }
-    
+
 
 print_r($object);
 
@@ -88,122 +89,108 @@ offsetSetTest();
 
 stdClass Object
 (
-    [a] => stdClass Object
-        (
-            [a_1] => a 1 first
-            [a_2] => a 2 first
-        )
-
+    [a] => a first
     [b] => stdClass Object
         (
-            [b_1] => stdClass Object
-                (
-                    [b_1_1] => b 1 1 first
-                    [b_1_2] => b 1 2 first
-                    [b_1_3] => b 1 3 first
-                )
-
-            [b_2] => 0
-            [b_3] => 
-            [b_4] => Array
-                (
-                )
-
-            [b_5] => stdClass Object
-                (
-                )
-
-            [a] => stdClass Object
-                (
-                    [a_1] => a 1 second
-                    [a_2] => a 2 second
-                )
-
-            [b] => stdClass Object
-                (
-                    [b_1] => stdClass Object
-                        (
-                            [b_1_1] => b 1 1 second
-                            [b_1_2] => b 1 2 second
-                            [b_1_3] => b 1 3 second
-                        )
-
-                    [b_2] => 0
-                    [b_3] => 
-                    [b_4] => Array
-                        (
-                        )
-
-                    [b_5] => stdClass Object
-                        (
-                        )
-                )
-
-            [new] => Jekyll
+            [b_1] => b_1 first
         )
 
-    [new] => Jekyll
+    [c] => stdClass Object
+        (
+            [c_1] => c_1 first
+            [c_2] => c_2 first
+        )
+
+    [d] => stdClass Object
+        (
+            [d_1] => stdClass Object
+                (
+                    [d_1_1] => d_1_1 first
+                    [d_1_2] => d_1_2 first
+                    [d_1_3] => d_1_3 first
+                    [new] => Jekyll
+                )
+        )
+
+    [e] => stdClass Object
+        (
+            [a] => a first
+            [b] => stdClass Object
+                (
+                    [b_1] => b_1 second
+                )
+
+            [c] => stdClass Object
+                (
+                    [c_1] => c_1 second
+                    [c_2] => c_2 second
+                )
+
+            [d] => stdClass Object
+                (
+                    [d_1] => stdClass Object
+                        (
+                            [d_1_1] => d_1_1 second
+                            [d_1_2] => d_1_2 second
+                            [d_1_3] => d_1_3 second
+                            [new] => Jekyll
+                        )
+                )
+        )
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 stdClass Object
 (
-    [a] => stdClass Object
-        (
-            [a_1] => a 1 first
-            [a_2] => a 2 first
-        )
-
+    [a] => a first
     [b] => stdClass Object
         (
-            [b_1] => stdClass Object
-                (
-                    [b_1_1] => b 1 1 first
-                    [b_1_2] => b 1 2 first
-                    [b_1_3] => b 1 3 first
-                )
-
-            [b_2] => 0
-            [b_3] => 
-            [b_4] => Array
-                (
-                )
-
-            [b_5] => stdClass Object
-                (
-                )
-
-            [a] => stdClass Object
-                (
-                    [a_1] => a 1 second
-                    [a_2] => a 2 second
-                )
-
-            [b] => stdClass Object
-                (
-                    [b_1] => stdClass Object
-                        (
-                            [b_1_1] => b 1 1 second
-                            [b_1_2] => b 1 2 second
-                            [b_1_3] => b 1 3 second
-                        )
-
-                    [b_2] => 0
-                    [b_3] => 
-                    [b_4] => Array
-                        (
-                        )
-
-                    [b_5] => stdClass Object
-                        (
-                        )
-                )
-
-            [new] => Hyde
+            [b_1] => b_1 first
         )
 
-    [new] => Hyde
+    [c] => stdClass Object
+        (
+            [c_1] => c_1 first
+            [c_2] => c_2 first
+        )
+
+    [d] => stdClass Object
+        (
+            [d_1] => stdClass Object
+                (
+                    [d_1_1] => d_1_1 first
+                    [d_1_2] => d_1_2 first
+                    [d_1_3] => d_1_3 first
+                    [new] => Hyde
+                )
+        )
+
+    [e] => stdClass Object
+        (
+            [a] => a first
+            [b] => stdClass Object
+                (
+                    [b_1] => b_1 second
+                )
+
+            [c] => stdClass Object
+                (
+                    [c_1] => c_1 second
+                    [c_2] => c_2 second
+                )
+
+            [d] => stdClass Object
+                (
+                    [d_1] => stdClass Object
+                        (
+                            [d_1_1] => d_1_1 second
+                            [d_1_2] => d_1_2 second
+                            [d_1_3] => d_1_3 second
+                            [new] => Hyde
+                        )
+                )
+        )
 )
 
 */
