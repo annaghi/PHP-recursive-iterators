0213c8e0236ee763474414d1de509067b557e0fe
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-20 06:50:56 +0100

    Update RecursiveArrayIterator::hasChildren.php

--- RecursiveArrayIterator::hasChildren.php
+++ RecursiveArrayIterator::hasChildren.php
@@ -6,38 +6,20 @@ function hasChildrenTest() {
 
     $array = array (
 
-            'a' => 'a first',
+            'a' => 'a text',
+
             'b' => array(
-                    'b_1' => 'b_1 first',
+                    'b_1' => 'b_1 text',
+                    'b_2' => 'b_2 text',
             ),
+
             'c' => array(
-                    'c_1' => 'c_1 first',
-                    'c_2' => 'c_2 first',
-            ),
-            'd' => array(
-                    'd_1' => array(
-                            'd_1_1' => 'd_1_1 first',
-                            'd_1_2' => 'd_1_2 first',
-                            'd_1_3' => 'd_1_3 first',
+                    'c_1' => array(
+                            'c_1_1' => 'c_1_1 text',
                     ),
-            ),
-
-            'e' => array(
-
-                    'a' => 'a second',
-                    'b' => array(
-                            'b_1' => 'b_1 second',
-                    ),
-                    'c' => array(
-                            'c_1' => 'c_1 second',
-                            'c_2' => 'c_2 second',
-                    ),
-                    'd' => array(
-                            'd_1' => array(
-                                    'd_1_1' => 'd_1_1 second',
-                                    'd_1_2' => 'd_1_2 second',
-                                    'd_1_3' => 'd_1_3 second',
-                            ),
+                    'c_2' => array(
+                            'c_2_1' => 'c_2_1 text',
+                            'c_2_2' => 'c_2_2 text',
                     ),
             ),
     );
@@ -68,7 +50,7 @@ print_r($object);
 
     foreach( $iterator as $key => $current ) {
 
-        if( $iterator->getInnerIterator()->hasChildren() && $iterator->getInnerIterator()->count() == 1 ) {
+        if( $iterator->getInnerIterator()->hasChildren() && $iterator->getInnerIterator()->count() == 2 ) {
 
             $iterator->getInnerIterator()->offsetSet( $key, 'NODE' );
         }
@@ -93,46 +75,20 @@ stdClass Object
     [b] => stdClass Object
         (
             [b_1] => LEAF
+            [b_2] => LEAF
         )
 
     [c] => stdClass Object
         (
-            [c_1] => LEAF
-            [c_2] => LEAF
-        )
-
-    [d] => stdClass Object
-        (
-            [d_1] => stdClass Object
+            [c_1] => stdClass Object
                 (
-                    [d_1_1] => LEAF
-                    [d_1_2] => LEAF
-                    [d_1_3] => LEAF
-                )
-        )
-
-    [e] => stdClass Object
-        (
-            [a] => LEAF
-            [b] => stdClass Object
-                (
-                    [b_1] => LEAF
+                    [c_1_1] => LEAF
                 )
 
-            [c] => stdClass Object
+            [c_2] => stdClass Object
                 (
-                    [c_1] => LEAF
-                    [c_2] => LEAF
-                )
-
-            [d] => stdClass Object
-                (
-                    [d_1] => stdClass Object
-                        (
-                            [d_1_1] => LEAF
-                            [d_1_2] => LEAF
-                            [d_1_3] => LEAF
-                        )
+                    [c_2_1] => LEAF
+                    [c_2_2] => LEAF
                 )
         )
 )
@@ -145,37 +101,13 @@ stdClass Object
     [b] => stdClass Object
         (
             [b_1] => LEAF
+            [b_2] => LEAF
         )
 
     [c] => stdClass Object
         (
-            [c_1] => LEAF
-            [c_2] => LEAF
-        )
-
-    [d] => stdClass Object
-        (
-            [d_1] => NODE
-        )
-
-    [e] => stdClass Object
-        (
-            [a] => LEAF
-            [b] => stdClass Object
-                (
-                    [b_1] => LEAF
-                )
-
-            [c] => stdClass Object
-                (
-                    [c_1] => LEAF
-                    [c_2] => LEAF
-                )
-
-            [d] => stdClass Object
-                (
-                    [d_1] => NODE
-                )
+            [c_1] => NODE
+            [c_2] => NODE
         )
 )
 
