886a851f56feca69e5a0ffcd857279e9c4795f3c
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-20 14:22:41 +0100

    Update array_to_object.php

--- array_to_object.php
+++ array_to_object.php
@@ -1,4 +1,11 @@
 <?php
 
-$object = json_decode( json_encode( $array ));
+function array_to_object( $array ) {
+    if( is_array( $array )) {
+        return (object) array_map( __FUNCTION__, $array );
+    }
+    else {
+        return $array;
+    }
+}
 
