eec42d2237b5dfb61a678e27312058b5bef75896
Anna Bansaghi <anna.bansaghi@gmail.com>
2013-03-20 06:24:51 +0100

    Create RecursiveIteratorIterator::getInnerIterator.php

---
+++ RecursiveIteratorIterator::getInnerIterator.php
@@ -0,0 +1,191 @@
+<?php
+
+//A simple use case of RecursiveIteratorIterator::getInnerIterator method
+
+function getInnerIteratorTest() {
+
+    $array = array (
+
+            'a' => 'a text',
+
+            'b' => array(
+                    'b_1' => 'b_1 text',
+                    'b_2' => 'b_2 text',
+            ),
+
+            'c' => array(
+                    'c_1' => array(
+                            'c_1_1' => 'c_1_1 text',
+                    ),
+                    'c_2' => array(
+                            'c_2_1' => 'c_2_1 text',
+                            'c_2_2' => 'c_2_2 text',
+                    ),
+
+            ),
+
+    );
+
+
+
+    $object   = json_decode( json_encode( $array ));
+//**    $object   = new ArrayObject( $array, 0, "RecursiveArrayIterator" );
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::LEAVES_ONLY );
+
+    foreach( $iterator as $key => $current ) {
+
+        if( $iterator->getInnerIterator()->count() == 2 ) {
+
+log_it($iterator->getInnerIterator());
+
+        }
+    }
+
+
+log_it('---------------------------------------------------------------------------------------------------------------------');
+
+
+    $iterator = new RecursiveIteratorIterator( new RecursiveArrayIterator( $object ), RecursiveIteratorIterator::SELF_FIRST );
+
+    foreach( $iterator as $key => $current ) {
+
+        if( $iterator->getInnerIterator()->count() == 2 ) {
+
+log_it($iterator->getInnerIterator());
+
+        }
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
+
+getInnerIteratorTest();
+
+
+
+/*
+
+RecursiveArrayIterator Object
+(
+    [storage:ArrayIterator:private] => stdClass Object
+        (
+            [b_1] => b_1 text
+            [b_2] => b_2 text
+        )
+)
+
+
+RecursiveArrayIterator Object
+(
+    [storage:ArrayIterator:private] => stdClass Object
+        (
+            [b_1] => b_1 text
+            [b_2] => b_2 text
+        )
+)
+
+
+RecursiveArrayIterator Object
+(
+    [storage:ArrayIterator:private] => stdClass Object
+        (
+            [c_2_1] => c_2_1 text
+            [c_2_2] => c_2_2 text
+        )
+)
+
+
+RecursiveArrayIterator Object
+(
+    [storage:ArrayIterator:private] => stdClass Object
+        (
+            [c_2_1] => c_2_1 text
+            [c_2_2] => c_2_2 text
+        )
+)
+
+--------------------------------------------------------------------------------------------------------------------------------
+
+RecursiveArrayIterator Object
+(
+    [storage:ArrayIterator:private] => stdClass Object
+        (
+            [b_1] => b_1 text
+            [b_2] => b_2 text
+        )
+)
+
+
+RecursiveArrayIterator Object
+(
+    [storage:ArrayIterator:private] => stdClass Object
+        (
+            [b_1] => b_1 text
+            [b_2] => b_2 text
+        )
+)
+
+
+RecursiveArrayIterator Object
+(
+    [storage:ArrayIterator:private] => stdClass Object
+        (
+            [c_1] => stdClass Object
+                (
+                    [c_1_1] => c_1_1 text
+                )
+
+            [c_2] => stdClass Object
+                (
+                    [c_2_1] => c_2_1 text
+                    [c_2_2] => c_2_2 text
+                )
+        )
+)
+
+
+RecursiveArrayIterator Object
+(
+    [storage:ArrayIterator:private] => stdClass Object
+        (
+            [c_1] => stdClass Object
+                (
+                    [c_1_1] => c_1_1 text
+                )
+
+            [c_2] => stdClass Object
+                (
+                    [c_2_1] => c_2_1 text
+                    [c_2_2] => c_2_2 text
+                )
+        )
+)
+
+
+RecursiveArrayIterator Object
+(
+    [storage:ArrayIterator:private] => stdClass Object
+        (
+            [c_2_1] => c_2_1 text
+            [c_2_2] => c_2_2 text
+        )
+)
+
+
+RecursiveArrayIterator Object
+(
+    [storage:ArrayIterator:private] => stdClass Object
+        (
+            [c_2_1] => c_2_1 text
+            [c_2_2] => c_2_2 text
+        )
+)
+
+*/
+
